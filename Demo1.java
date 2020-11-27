c48d57d8b94a830c16449ae6a3bcd8feff563dab
pkumar <p.altruist@yahoo.com>
2020-11-27 04:40:07 +0000

    commit demo1

--- Demo1.java
+++ Demo1.java
@@ -1,8 +1,8 @@
 class Demo1
 {
 	public static void main(String args[]){
-		system.out.println("This is Demo1 java class");
-	        system.out.println("This is 2nd print for Demo1 class file");
+	        System.out.println("This is Demo1 java class");
+	        System.out.println("This is 2nd print for Demo1 class file");
 	}
 }
 
